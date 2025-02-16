2f88d90f7b3f712adf8f7d3e540dcf4356c195dc
Abhishek Bhagat <82287046+beatbeast007@users.noreply.github.com>
2025-02-17 05:14:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionBottom = section.getBoundingClientRect().bottom;
+        if (sectionTop < window.innerHeight && sectionBottom > 0) {
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+    });
+});
